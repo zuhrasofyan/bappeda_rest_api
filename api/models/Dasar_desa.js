09eb372bc3cca2853d82a80dad5a11bca1e07160
Zuhra Sofyan <zuhrasofyan@gmail.com>
2017-01-10 12:00:20 +0700

    add Dasar_desa model

---
+++ api/models/Dasar_desa.js
@@ -0,0 +1,53 @@
+/**
+ * Dasar_desa.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  //use the format defined in database instead
+  migrate: 'safe',
+
+  //disable create autoCreateAt & autoUpdatedAt column in table
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+
+  attributes: {
+    DESA_ID: {
+      type: 'INTEGER',
+      primaryKey: true,
+      required: true,
+      unique: true
+    },
+    PROVINSI_ID: {
+      type: 'INTEGER',
+      required: true
+    },
+    KAB_ID: {
+      type: 'INTEGER',
+      required: true
+    },
+    KECAMATAN_ID: {
+      type: 'INTEGER',
+      required: true
+    },
+    NAMA: {
+      type: 'STRING',
+      required: true
+    },
+    KELILING_M2: {
+      type: 'FLOAT'
+    },
+    LUAS_M2: {
+      type: 'FLOAT'
+    },
+    LINTANG: {
+      type: 'FLOAT'
+    },
+    BUJUR: {
+      type: 'FLOAT'
+    }
+  }
+};
+
