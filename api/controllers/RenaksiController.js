598154cc9c36f6632cde29076f2bcebb458430b4
zuhrasofyan <zuhra.sofyan@rwth-aachen.de>
2017-03-16 11:42:19 +0700

    Initial controller for renaksi

---
+++ api/controllers/RenaksiController.js
@@ -0,0 +1,11 @@
+/**
+ * RenaksiController
+ *
+ * @description :: Server-side logic for managing Renaksis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
