076ab73c7cae8dc771dfbfc8a452c95dcea63705
zuhrasofyan <zuhrasofyan@gmail.com>
2017-05-18 10:34:28 +0700

    Create bukti_renaksi model

---
+++ api/models/Bukti_renaksi.js
@@ -0,0 +1,22 @@
+/**
+ * Bukti_renaksi.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    renaksiId: {
+      model: 'renaksi'
+    },
+    imageName: {
+      type: 'STRING'
+    },
+    imageFd: {
+      type: 'STRING'
+    }
+  }
+};
+
