8d8ad931b69933253dcebd61ea549a71503a6729
zuhrasofyan <zuhrasofyan@gmail.com>
2017-06-14 12:40:46 +0700

    Create SKPD list model

---
+++ api/models/Skpd.js
@@ -0,0 +1,19 @@
+/**
+ * Skpd.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    skpd: {
+      type: 'STRING',
+      unique: true
+    }
+  },
+  autoCreatedAt: false,
+  autoUpdatedAt: false
+};
+
