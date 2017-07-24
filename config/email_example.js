1e9aa8e366611b207a7c31c785ee88620b03995c
zuhrasofyan <zuhrasofyan@gmail.com>
2017-07-24 15:43:57 +0700

    .

--- config/email_example.js
+++ config/email_example.js
@@ -1,4 +1,5 @@
-//!IMPORTANT! uncomment, Fill the right info and change the name to email.js this file for production
+// !IMPORTANT! uncomment, Fill the right info and change the name to email.js this file for production
+// testMode: true not gonna send registered info an email. instead it will be written to a '.tmp/email.txt' file
 // module.exports.email = {
 //     service: 'Gmail',
 //     auth: {user: 'YOUREMAIL', pass: 'YOURPASS'},
