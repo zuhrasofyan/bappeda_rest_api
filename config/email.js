8a43ac438daaf27316b931c2845420e5f445b6ce
zuhrasofyan <zuhrasofyan@gmail.com>
2017-07-24 15:33:37 +0700

    ignore config

--- config/email.js
+++
@@ -1,8 +0,0 @@
-//!IMPORTANT! change this credential for production
-module.exports.email = {
-    service: 'Gmail',
-    auth: {user: 'giscbappeda.bna@gmail.com', pass: 'OFCOURSE!ITS!SECRET'},
-    templateDir: 'api/emailTemplates',
-    from: 'giscbappeda.bna@gmail.com',
-    testMode: false
-};
