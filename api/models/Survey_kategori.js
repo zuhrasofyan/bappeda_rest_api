9881ad96e99c330f648d2c01b8f85042a21f71bf
Zuhra <zuhrasofyan@gmail.com>
2019-01-18 10:26:37 +0700

    add survey kategory model

--- api/models/Survey_kategori.js
+++ api/models/Survey_kategori.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   //use the format defined in database instead
-  migrate: 'safe',
+  // migrate: 'safe',
 
   autoCreatedAt: false,
   autoUpdatedAt: false,
@@ -20,9 +20,15 @@ module.exports = {
     },
 
     // add reference to pertanyaan
-    pertanyaan: {
+    pertanyaans: {
       collection: 'survey_pertanyaan_layak',
-      via: 'kategoris'
+      via: 'kategori'
+    },
+
+    // add reference to jawaban
+    jawabans: {
+      collection: 'survey_jawaban_layak_huni',
+      via: 'kategori'
     }
 
   },
