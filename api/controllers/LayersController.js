95e94729be813c2d33b611db18c1c7de8730d206
Zuhra <zuhrasofyan@gmail.com>
2019-05-24 10:42:38 +0700

    sails.io publishCreate new Layer

--- api/controllers/LayersController.js
+++ api/controllers/LayersController.js
@@ -18,6 +18,7 @@ module.exports = {
 			if (err) {
 				return res.serverError(err);
 			} else {
+				Layers.publishCreate(result);
 				return res.ok();
 			}
 		});
@@ -54,9 +55,9 @@ module.exports = {
 				} else {
 						Layers.update(layerId, {isDeleted: true}).exec(function(err, result){
 							if (err) {
-									return res.serverError(err);
+								return res.serverError(err);
 							} else {
-									return res.ok('layer berhasil di nonaktifkan');
+								return res.ok('layer berhasil di nonaktifkan');
 							}
 						})
 				}
@@ -166,6 +167,69 @@ module.exports = {
 				})
 			}
 		})
-	}
+	},
+
+	subscribe: function(req, res) {
+    if( !req.isSocket) {
+      return res.badRequest();
+    }
+    Layers.find().exec(function (err, layers) {
+      if (err) {
+        return res.serverError({ status: 'SERVER_ERROR' });
+      }
+      /**
+       * subscribe the Layers model to connected client sockets
+       */
+      Layers.subscribe(req, _.pluck(layers, 'id'));
+      Layers.watch(req);
+      return res.ok({ status: 'SUBSCRIBED' });
+    });
+  },
+
+	// //test socket
+	// hello: function(req, res) {
+
+  //   // Make sure this is a socket request (not traditional HTTP)
+  //   if (!req.isSocket) {
+  //     return res.badRequest();
+  //   }
+
+  //   // Have the socket which made the request join the "funSockets" room.
+  //   sails.sockets.join(req, 'funSockets');
+
+  //   // Broadcast a notification to all the sockets who have joined
+  //   // the "funSockets" room, excluding our newly added socket:
+	// 	sails.sockets.broadcast('funSockets', 'hello', { howdy: 'hi there!'}, req);
+	// 	var socketId = sails.sockets.getId(req);
+	// 	// => "BetX2G-2889Bg22xi-jy"
+
+	// 	sails.log('My socket ID is: ' + socketId);
+
+  //   // ^^^
+  //   // At this point, we've blasted out a socket message to all sockets who have
+  //   // joined the "funSockets" room.  But that doesn't necessarily mean they
+  //   // are _listening_.  In other words, to actually handle the socket message,
+  //   // connected sockets need to be listening for this particular event (in this
+  //   // case, we broadcasted our message with an event name of "hello").  The
+  //   // client-side code you'd need to write looks like this:
+  //   // 
+  //   //   io.socket.on('hello', function (broadcastedData){
+  //   //       console.log(data.howdy);
+  //   //       // => 'hi there!'
+  //   //   }
+  //   // 
+
+  //   // Now that we've broadcasted our socket message, we still have to continue on
+  //   // with any other logic we need to take care of in our action, and then send a
+  //   // response.  In this case, we're just about wrapped up, so we'll continue on
+
+  //   // Respond to the request with a 200 OK.
+  //   // The data returned here is what we received back on the client as `data` in:
+  //   // `io.socket.get('/say/hello', function gotResponse(data, jwRes) { /* ... */ });`
+  //   return res.json({
+  //     anyData: 'we want to send back'
+  //   });
+
+	// }
 };
 
